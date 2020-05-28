e4ca16d573ab9ba38e473746742526a9f9978380
Muhamad Amir <m.amirazmi@hotmail.com>
2020-05-28 16:25:35 +0800

    Database class 2

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -5,8 +5,10 @@
 		}
 
 		public function index(){
+			$posts = $this->postModel->getPosts();
 			$data = [
-				"title" => "Welcome"
+				"title" => "Welcome", 
+				"posts" => $posts
 			];
 			$this->view("pages/index", $data);
 		}
